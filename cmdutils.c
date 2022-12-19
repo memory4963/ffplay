66a1d27d94533e8ad6bb56d39fa84281391d400d
memory4963 <bolo4963@gmail.com>
2022-12-19 18:16:07 +0900

    [update] C->C++

--- cmdutils.c
+++ cmdutils.c
@@ -166,16 +166,16 @@ void show_help_options(const OptionDef *options, const char *msg, int req_flags,
     printf("\n");
 }
 
-void show_help_children(const AVClass *class, int flags)
+void show_help_children(const AVClass *clazz, int flags)
 {
     void *iter = NULL;
     const AVClass *child;
-    if (class->option) {
-        av_opt_show2(&class, NULL, flags, 0);
+    if (clazz->option) {
+        av_opt_show2(&clazz, NULL, flags, 0);
         printf("\n");
     }
 
-    while (child = av_opt_child_class_iterate(class, &iter))
+    while (child = av_opt_child_class_iterate(clazz, &iter))
         show_help_children(child, flags);
 }
 
